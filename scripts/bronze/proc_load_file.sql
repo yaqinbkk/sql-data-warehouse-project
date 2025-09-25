4f3700a239861eb12d54ac9b26f52af5dd689a50
yaqinbkk <34959362+yaqinbkk@users.noreply.github.com>
2025-09-25 21:10:03 +0700

    Create proc_load_file.sql

---
+++ scripts/bronze/proc_load_file.sql
@@ -0,0 +1,98 @@
+/*
+==============================================================================================================================================================================
+Stored Procedure: Load Bronze Layer (Source --> Bronze)
+==============================================================================================================================================================================
+Script Purpose:
+  This stored procedure load data into the 'bronze' schema from external csv files.
+  It performs the following actions:
+    - Truncate the bronze tables before loading data.
+    - Use the 'BULK INSERT' command to load data from csv files to bronze tables.
+Parameters:
+None.
+This stored procedure does not accept any parameters or return any values.
+
+Usage example:
+  EXEC bronze.load_bronze;
+==============================================================================================================================================================================
+*/
+
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+	DECLARE @start_time DATETIME, @end_time DATETIME; -- This helps us check how long it takes for each table to be loaded -- SET start, SET end and PRINT the outcome --
+	SET @start_time = GETDATE();
+	TRUNCATE TABLE bronze.crm_cust_info;
+
+	BULK INSERT bronze.crm_cust_info FROM 'C:\Users\yaqeenbkk\Divers\Coding\SQL\SQL Course (30 hours)\SQL Projects\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+	SET @end_time = GETDATE();
+	PRINT '>> Load duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+
+
+	SET @start_time = GETDATE();
+	TRUNCATE TABLE bronze.crm_prd_info;
+
+	BULK INSERT bronze.crm_prd_info FROM 'C:\Users\yaqeenbkk\Divers\Coding\SQL\SQL Course (30 hours)\SQL Projects\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+	SET @end_time = GETDATE();
+	PRINT '>> Load duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+
+
+	SET @start_time = GETDATE();
+	TRUNCATE TABLE bronze.crm_sales_details;
+
+	BULK INSERT bronze.crm_sales_details FROM 'C:\Users\yaqeenbkk\Divers\Coding\SQL\SQL Course (30 hours)\SQL Projects\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+	SET @end_time = GETDATE();
+	PRINT '>> Load duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+
+
+	SET @start_time = GETDATE();
+	TRUNCATE TABLE bronze.erp_cust_az12;
+
+	BULK INSERT bronze.erp_cust_az12 FROM 'C:\Users\yaqeenbkk\Divers\Coding\SQL\SQL Course (30 hours)\SQL Projects\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+	SET @end_time = GETDATE();
+	PRINT '>> Load duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+
+
+	SET @start_time = GETDATE();
+	TRUNCATE TABLE bronze.erp_loc_a101;
+
+	BULK INSERT bronze.erp_loc_a101 FROM 'C:\Users\yaqeenbkk\Divers\Coding\SQL\SQL Course (30 hours)\SQL Projects\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+	SET @end_time = GETDATE();
+	PRINT '>> Load duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+
+
+	SET @start_time = GETDATE();
+	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+	BULK INSERT bronze.erp_px_cat_g1v2 FROM 'C:\Users\yaqeenbkk\Divers\Coding\SQL\SQL Course (30 hours)\SQL Projects\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+	SET @end_time = GETDATE();
+	PRINT '>> Load duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+END
